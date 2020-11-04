c0f68f13e8b4e670e88b0a41cd9c4184bcc2d6db
BrandonCorn <corn_brandon@columbusstate.edu>
2020-11-04 18:11:31 -0500

    example request for games implemented

--- controllers/search.js
+++ controllers/search.js
@@ -2,18 +2,22 @@ const axios = require('axios');
 const config = require('../config/keys'); 
 
 const searchGameName = async (token) => {
-    return new Promise( (resolve, reject) => {
-        const creds = {
+    return new Promise( async (resolve, reject) => {
+        const bearer = 'Bearer ' + token; 
+        axios({
+            url: 'https://api.igdb.com/v4/games/', 
+            method: 'POST', 
             headers: {
-                'Client-ID': config.IGDB_CLIENT_ID, 
-                'Authorization': `Bearer ${token}`
-            }
-        }
-
-        axios.post('https://api.igdb.com/v4/games', {}, creds)
-        .then( res => {
-            console.log(res.data);
-            resolve(res.data); 
+                'Accept': 'application/json',  
+                'Client-ID': config.IGDB_CLIENT_ID,
+                'Authorization': bearer, 
+                'Content-Type': 'text/plain',
+            },
+            data: 'fields name; where name ~ "a"*; limit 20;'
+        })
+        .then( apiRes => {
+            console.log(apiRes);
+            resolve(apiRes.data);  
         })
         .catch( err => {
             console.log(err); 
