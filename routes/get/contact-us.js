7dec1628c0a4897e2e5834029204f0e6a4a685d1
BrandonCorn <corn_brandon@columbusstate.edu>
2020-11-09 13:37:35 -0500

    create contact-us page and setup format for page

---
+++ routes/get/contact-us.js
@@ -0,0 +1,6 @@
+const { render } = require("ejs");
+
+module.exports = (req, res) => {
+
+    return res.render('contact-us', {}); 
+}
--- routes/get/index.js
+++ routes/get/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router(); 
 
 router.get('/', require('./home')); 
+router.get('/contact-us', require('./contact-us')); 
 
 module.exports = router; 
